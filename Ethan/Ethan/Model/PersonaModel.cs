b5072a638f04bee8a6ebb44b0c969c910240fe4f
unknown <diazethan@hotmail.com>
2021-08-20 07:02:37 -0600

    Examen

---
+++ Ethan/Ethan/Model/PersonaModel.cs
@@ -0,0 +1,90 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+namespace Ethan.Model
+{
+    public class PersonaModel:INotifyPropertyChanged
+    {
+        private int IdPago;
+        private string Descripcion;
+        private double Monto;
+        private DateTime Fecha;
+        private string foto;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propiedad) 
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this,new PropertyChangedEventArgs(propiedad));
+            }
+        }
+
+        [PrimaryKey,AutoIncrement]
+        public int IdPag
+        {
+            get { return IdPag; }
+            set
+            {
+                if(IdPag !=value)
+                {
+                    IdPag = value;
+                    OnPropertyChanged("IdPag");
+                }
+            }
+        }
+        public string Descrip
+        {
+            get { return Descrip; }
+            set
+            {
+                if (Descrip != value)
+                {
+                    Descrip = value;
+                    OnPropertyChanged("Descrip");
+                }
+            }
+        }
+        public string Fech
+        {
+            get { return Fech; }
+            set
+            {
+                if (Fech != value)
+                {
+                    Fech = value;
+                    OnPropertyChanged("Fech");
+                }
+            }
+        }
+        public string Mont
+        {
+            get { return Mont; }
+            set
+            {
+                if (Mont != value)
+                {
+                    Mont = value;
+                    OnPropertyChanged("Mont");
+                }
+            }
+        }
+        public string Fot
+        {
+            get { return Fot; }
+            set
+            {
+                if (Fot != value)
+                {
+                    Fot = value;
+                    OnPropertyChanged("Fot");
+                }
+            }
+        }
+
+
+    }
+}
---
+++ Ethan/Ethan/Paginas/Persona.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Ethan.Paginas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Persona : ContentPage
+    {
+        public Persona()
+        {
+            InitializeComponent();
+            btnVer.Clicked += BtnVer_Clicked;
+        }
+
+        private void BtnVer_Clicked(object sender, EventArgs e)
+        {
+            ((NavigationPage)this.Parent).PushAsync(new Listado());
+        }
+    }
+}
